5340917499094873ec607a443c8a05c3ee9e8848
André Joarez <andre.ncardoso@hotmail.com>
2023-02-14 10:54:38 -0300

    s

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Eeedit <code>src/App.js</code> and save to reload.
+          Eeessdit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
