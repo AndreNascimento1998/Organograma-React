b947cee417a6f99e1c652f22e94bbdffff426957
André Joarez <andre.ncardoso@hotmail.com>
2023-02-17 14:43:23 -0300

    Arrumando

--- src/components/CardTeams/index.js
+++ src/components/CardTeams/index.js
@@ -1,10 +1,11 @@
 import './CardTeams.css';
 
 const CardTeams = (props) => {
+
     return(
         <div>
-            <img height='150px' width='150px' src='./image/pa.png'/>
-                <h5 className='p-1' style={{backgroundColor: props.corH5}}>Phantom Assasin</h5> 
+            <img height='150px' width='150px' src={props.dadosHeroi.url}/>
+                <h5 className='p-1' style={{backgroundColor:props.corH5}}>{props.dadosHeroi.nome}</h5> 
             <div className="card-img-overlay">
             </div>
         </div>
--- src/components/Teams/index.js
+++ src/components/Teams/index.js
@@ -3,19 +3,22 @@ import CardTeams from '../CardTeams';
 
 const Teams = (props) => {
 
+    const heroi = {
+        nome: 'Spectre',
+        url: './image/spectre.png'
+    }
 
     return (
         <div className='row centro gx-0'>
             <div className='col-9 '>
                 <div className='card text-center ' style={{backgroundColor: props.corPrimaria}}>
-                    <h1>{props.nome}</h1>
+                    <h1 className='pb-2' style={{backgroundColor: props.corSecundaria}}>{props.nome}</h1>
                     <div className='row p-1 text-center borderN'>
-                        <div className='col-3'>
-                            <div className='card border border-dark' style={{backgroundColor: props.corSecundaria}}>
-                                <CardTeams corH5={props.corPrimaria}/>
+                        <div className='col-3 mt-2'>
+                            <div className='card border border-dark-subtle' style={{backgroundColor: props.corSecundaria}}>
+                                <CardTeams dadosHeroi={heroi} corH5={props.corPrimaria}/>
                             </div>
                         </div>
-                        
                     </div> 
                 </div>
             </div>
